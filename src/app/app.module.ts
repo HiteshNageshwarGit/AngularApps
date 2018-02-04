f9524be169a591faf6ea051475490da21d81ec47
HiteshNageshwar <nageshwarhitesh@gmail.com>
2018-02-04 09:45:07 -0500

    removed test modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { SiteHeaderComponent } from './site-header/site-header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
